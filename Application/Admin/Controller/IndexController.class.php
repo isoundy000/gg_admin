456e908b23738565c51239c1a72b59613f0ae708
jelly074100209 <419756656@qq.com>
2016-12-21 23:24:29 +0800

    杠杠麻将

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,6 +3,10 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->show('abc');
+        $MongoClient = new \MongoDB\Client(C('MONGO_SERVER'));
+        $MongoClient->selectDatabase(C('MONGO_DB'));
+        $user = $MongoClient->gg_admin->user;
+        $result = $user->findOne();
+        var_dump($result);
     }
 }
--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,6 +1,13 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+	//基本配置
     'MODULE_ALLOW_LIST' => array('Admin','Agent'),
-    'DEFAULT_MODULE' => 'Admin'
+    'DEFAULT_MODULE' => 'Admin', //默认模块
+    'URL_CASE_INSENSITIVE' => true, //URL不区分大小写
+    'URL_MODEL' => 2, //PATH_INFO模式 http://www.example.com/admin/user
+
+    //数据库配置 mongodb
+    'MONGO_SERVER' => 'mongodb://127.0.0.1:27017',
+    'MONGO_DB' => 'gg_admin',
+
 );
